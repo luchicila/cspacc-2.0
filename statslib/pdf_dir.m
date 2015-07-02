c5cc1fbded78c62d8f565238278e95ef59633010
Andrei Luchici <luchici.andrei@gmail.com>
2015-07-02 07:55:54 +0100

    functions for computing pdf

--- statslib/pdf_dir.m
+++ statslib/pdf_dir.m
@@ -1,1 +1,40 @@
-function pdist_dir = pdf_dir()
+function [pdist_theta, x_theta, pdist_phi, x_phi] = pdf_dir()
+% Estimate the probability density function for vector directions. 
+% Input:
+%   vec = [nt x 3 x n] matrix with the (x, y, z) components of the vectors.
+%   n = number of vectors in the sample.
+%   nt = maximum number of time points.
+%
+% Output:
+%   pdist_theta = [nt x n, 1] vector with the probability density function
+%                 for the direction theta, between the XY projection of the 
+%                 vector and OX axis.
+%   x_theta = [nt x n, 1] vector with angles at which the PDF for theta is 
+%             evaluated.
+%   pdist_phi = [nt x n, 1] vector with the probability density function
+%               for the direction phi, between the vector and OZ axis.
+%   x_phi = [nt x n, 1] vector with angles at which the PDF for phi is 
+%           evaluated.
+% 
+% Author: A. Luchici, 2015.
+
+% Compute vector direction.
+vec_dir = compute_dir(vec, n, nt);
+
+theta = vec_dir(:,1,:);
+theta = reshape(theta, n*nt, 1);
+
+phi = vec_dir(:,2,:);
+phi = reshape(phi, n*nt, 1);
+
+% Fit distribution to vectors magnitude using Epanechnikov kernel.
+pd_theta = fitdist(theta, 'Kernel', 'Kernel', 'epanechnikov');
+pd_phi = fitdist(phi, 'Kernel', 'Kernel', 'epanechnikov');
+
+% Compute distribution values for evenly spaced magnitudes between 0 and
+% maximum recorded magnitude + 10% more.
+x_theta = linspace(0,max(theta)+0.1*max(theta),100);
+pdist_theta = pdf(pd_theta, x_theta);
+
+x_phi = linspace(min(phi)-0.1*min(phi), max(phi)+0.1*max(phi),100);
+pdist_phi = pdf(pd_phi, x_phi);
